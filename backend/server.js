42e0fc9f6221ae9bca7fc8b15eb87e39ce55ba59
Bekheit <muhammadali9765@gmail.com>
2021-09-11 17:19:16 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,6 @@ const app = express()
 app.use(express.json())
 
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
-
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
-
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
 if (process.env.NODE_ENV === 'production'){
   console.log('heere')
   app.use(express.static(path.join(__dirname, '/frontend/build')))
@@ -39,6 +29,16 @@ if (process.env.NODE_ENV === 'production'){
     res.send('API running ...')
   })
 }
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
